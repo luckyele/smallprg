950df84f87e4b55ab404c245ea290cddf3f0e99b
luckyung <541913141@qq.com>
2020-07-19 19:20:32 +0800

    add

--- miniprogram-2/pages/describe_page/describe_page.js
+++ miniprogram-2/pages/describe_page/describe_page.js
@@ -19,7 +19,7 @@ Page({
         let that = this;
         let host = 'http://127.0.0.1:8080/';
         wx.request({           
-          url: 'http://127.0.0.1:8080/'+options.name+'.json',
+          url: 'http://127.0.0.1:8080/' + options.name + '.json',
           data:{},
           method:'GET',
           success:function(res){
